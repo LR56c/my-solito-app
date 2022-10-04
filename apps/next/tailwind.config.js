dafc74021e408ecbeebce4c3b893b1c76d52a199
Mauricio Herrera Ortega <ronalli56@gmail.com>
2022-10-04 08:45:34 -0300

    upload public image

--- apps/next/tailwind.config.js
+++ apps/next/tailwind.config.js
@@ -1,14 +1,22 @@
-const { theme } = require('app/design/tailwind/theme')
+const { theme } = require( 'app/design/tailwind/theme' )
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+  content  : [
     './pages/**/*.{js,jsx,ts,tsx}',
     '../../packages/**/*.{js,jsx,ts,tsx}',
   ],
-  plugins: [require('nativewind/tailwind/css')],
+  plugins  : [
+    require( 'nativewind/tailwind/css' ),
+    require( "daisyui" ),
+  ],
   important: 'html',
-  theme: {
+  theme    : {
     ...theme,
-  },
+    screens: {
+      'xxs' : '320px',
+      'xs' : '480px',
+      ...theme.screens,
+    },
+  }
 }
--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -7,43 +7,6 @@ import { MotiLink } from 'solito/moti'
 export function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center p-3">
-      <H1>Welcome to Solito.</H1>
-      <View className="max-w-xl">
-        <P className="text-center">
-          Here is a basic starter to show you how you can navigate from one
-          screen to another. This screen uses the same code on Next.js and React
-          Native.
-        </P>
-        <P className="text-center">
-          Solito is made by{' '}
-          <A
-            href="https://twitter.com/fernandotherojo"
-            hrefAttrs={{
-              target: '_blank',
-              rel: 'noreferrer',
-            }}
-          >
-            Fernando Rojo
-          </A>
-          .
-        </P>
-        <P className="text-center">
-          NativeWind is made by{' '}
-          <A
-            href="https://twitter.com/mark__lawlor"
-            hrefAttrs={{
-              target: '_blank',
-              rel: 'noreferrer',
-            }}
-          >
-            Mark Lawlor
-          </A>
-          .
-        </P>
-      </View>
-      <View className="h-[32px]" />
-      <Row className="space-x-8">
-        <TextLink href="/user/fernando">Regular Link</TextLink>
         <MotiLink
           href="/user/fernando"
           animate={({ hovered, pressed }) => {
@@ -63,7 +26,6 @@ export function HomeScreen() {
             Moti Link
           </Text>
         </MotiLink>
-      </Row>
     </View>
   )
 }
